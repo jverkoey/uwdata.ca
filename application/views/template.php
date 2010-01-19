6f367bc9023e61e9a1defe6bb66e61e2c0dff721
Jeff Verkoeyen <jverkoey@gmail.com>
2010-01-19 03:17:32 -0500

    Uservoice.

--- application/views/template.php
+++ application/views/template.php
@@ -67,6 +67,31 @@
 
 <? if (IN_PRODUCTION) { ?>
 <script type="text/javascript">
+var uservoiceOptions = {
+  /* required */
+  key: 'uwdata',
+  host: 'uwdata.uservoice.com', 
+  forum: '38662',
+  showTab: true,  
+  /* optional */
+  alignment: 'left',
+  background_color:'#333', 
+  text_color: 'white',
+  hover_color: '#999',
+  lang: 'en'
+};
+
+function _loadUserVoice() {
+  var s = document.createElement('script');
+  s.setAttribute('type', 'text/javascript');
+  s.setAttribute('src', ("https:" == document.location.protocol ? "https://" : "http://") + "cdn.uservoice.com/javascripts/widgets/tab.js");
+  document.getElementsByTagName('head')[0].appendChild(s);
+}
+_loadSuper = window.onload;
+window.onload = (typeof window.onload != 'function') ? _loadUserVoice : function() { _loadSuper(); _loadUserVoice(); };
+</script>
+
+<script type="text/javascript">
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 </script>
